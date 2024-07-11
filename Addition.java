4296d5868f878350a4cf4fa825a5b19367470b43
BujjiNeela <bujjineela03@gmail.com>
2024-07-11 16:41:26 +0530

    commit - msg

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+public static void main(String args[])
+{
+int num1 =20;
+int num2 = 30;
+int add=num1+num2;
+System.out.println("The addition number"+add)
+}
+}
---
+++ Even.java
@@ -0,0 +1,15 @@
+class Even
+{
+public static void main(String args[])
+{
+int n1=20;
+if(n1%2==0)
+{
+System.out.println("Enter even number");
+}
+else
+{
+System.out.println("Enter odd number");
+}
+}
+}
